cce0a9ce4410543818b85c0767d1ab11f37d3d9a
VFlora72 <viveka.flora@bsu.edu>
2025-11-20 10:22:00 -0500

    Update base path in Vite configuration

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cardinal-books/', // CHANGE THIS to match your GitHub repo name!
+  base: '/Pbl2cardinalbooksweb/', // CHANGE THIS to match your GitHub repo name!
 })
